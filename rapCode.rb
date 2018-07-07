f6d2b81b6635e674067b5cb8c61db28d51e22b94
Grant Kazan <grantkazan@gmail.com>
2018-07-07 15:56:01 -0500

    first commit

---
+++ rapCode.rb
@@ -0,0 +1,219 @@
+# #
+# # good blog post idea:
+# # guess the rap song represented with code
+
+
+# # if you're having girl problems I feel bad for you son
+# # I got 99 problems but a bitch aint one
+
+# girl_problems = false
+# count = 0
+
+girl_problems = true
+num = 0
+
+99.times do
+  if girl_problems == true
+    feel = "bad for you son"
+    aint_one = "bitch"
+    num += 1
+  else
+  end
+end
+p "ive got #{num} problems"
+
+
+# # I got royalty got loyalty inside my dna
+# # cocaine quar
+
+my_dna = [
+  "loyalty", 
+  "royalty", 
+  "cocaine quarter piece",
+  "war",
+  "peace",
+  "power",
+  "poison",
+  "pain",
+  "joy",
+  "hustle",
+  "ambition",
+  "flow",
+  "realness",
+  "kill shit",
+  "millions",
+  "riches buildin",
+  "dark",
+  "evil",
+  "troublesome",
+  "heart",
+]
+
+your_dna = [
+  {hormones_switch: true},
+  {sucker_shit: true},
+  {daddy_prolly_snitched_heritage: true},
+]
+
+def our_dna(*)
+  p "my_dna " + "not for imitation"
+  p "your_dna " + "an abomination"
+end
+
+our_dna()
+
+
+
+# # meet me in the trap it's going down
+# # this one's my fave so far
+
+in_the = ["trap","mall","club"]
+def going_down?(location)
+  location.each do |x|
+    p "meet me in the " + x
+    p "it's going down"
+  end
+  p "every where you meet me guarantee to go down"
+end
+going_down?(in_the)
+
+
+
+# # hot in herre
+
+hot = true
+clothes = ["gucci collar","vokal tank top"]
+butt = "big"
+
+def in_here
+  if hot
+    clothes.pop
+  end
+end
+
+def check_reflection
+  if butt == "big"
+    tell_best_friend = true
+    return "girl I think my butt"
+  else
+  end
+end
+
+
+
+
+
+# # ride wit me
+shorty = {
+  age: 19, 
+  thicky_thick: true, 
+  measurements: 36-25-34,
+  wanna: true
+}
+
+
+def take_a_ride_with_me
+  if age == 18 or 19 and wanna == true
+    we = "3 wheeling in the fo" + " the gold D's"
+    while in_the_back_of_Benz-E 
+      smoke_l = true
+    end
+  end  
+end
+
+p "ayy must be the money"
+# # low
+
+shawty = {
+  "had_them"=> ["apple bottom jeans", "boots with the fur", "baggy sweat pants","reeboks with the straps",],
+  "got"=> [],
+}
+
+whole_club_lookin_at_her = true
+
+while whole_club_lookin_at_her do 
+  8.times do
+    shawty["got"] << "low" 
+  end  
+  puts "shawty #{shawty}"
+  break
+  return "hit_the_floor, next thing you know"   
+end
+
+# # ridin dirty
+
+def try_to_catch(_me_ridin)
+  while patrollin
+    if they_see
+      me_rollin == true
+      they_hatin == true
+      # if my_music_so_loud
+      #   swangin == true
+      #   dirty == true 
+      # end
+    end
+  end
+end
+
+# # ms jackson
+
+
+"daughter_cry" == true 
+
+def ms_jackson(_oh)
+  while true
+    "I_am_for_real" == true
+    100000000000.times do
+      "apologize" == true
+    end 
+  end
+end
+
+
+
+# drop it like it's hot
+
+
+# When the pimp's in the crib ma
+
+
+# Drop it like it's hot
+# Drop it like it's hot
+# Drop it like it's hot
+
+
+# When the pigs try to get at you
+
+
+# Park it like it's hot
+# Park it like it's hot
+# Park it like it's hot
+
+
+# And if a %%%%% get a attitude
+# Pop it like it's hot
+# Pop it like it's hot
+# Pop it like it's hot
+
+#awhile  pouring Chandon
+  # I got the Rolly on my arm
+#while got it going on
+# And I roll the best weed cause I got it going on
+
+# OMG this one is hysterical!!!
+p"%%%%%%%%%%%"
+when_the = [{"crib_mon"=> "when_the_pimps_in_the"},{"get_at_ya"=>"pigs"},{"attitude"=>"get_a"}]
+I = [{"got_the_rolly on my arm" => "pouring Chandon,"}, {"roll_the_best_weed"=>"got_it_going_on"}]
+"it" == "hot"
+when_the.each do |x|
+  p x
+  3.times do
+    p "drop it like it's hot"
+  end
+end
+I.each do |x|
+  p x
+end
+p"______"
+
+p "%%%%%%%%"
